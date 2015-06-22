555129062e9859b1934eefd5b981bf849574525f
Zenigata <lhaagounet@gmail.com>
2015-06-23 00:41:39 +0200

    Nettoyage images

--- javascripts/all.js
+++ javascripts/all.js
@@ -2345,21 +2345,11 @@ SCROLLING SCRIPTS
 ======================================*/
 
 $('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
-var $anchor = $(this);
-$('html, body').stop().animate({
-scrollTop: $($anchor.attr('href')).offset().top
-}, 1200, 'easeInOutExpo');
-event.preventDefault();
-});
-
-
-/*====================================
-SLIDER SCRIPTS
-======================================*/
-
-
-$('#carousel-slider').carousel({
-interval: 2000 //TIME IN MILLI SECONDS
+	var $anchor = $(this);
+	$('html, body').stop().animate({
+		scrollTop: $($anchor.attr('href')).offset().top
+	}, 1200, 'easeInOutExpo');
+	event.preventDefault();
 });
 
 
@@ -2367,13 +2357,13 @@ interval: 2000 //TIME IN MILLI SECONDS
 POPUP IMAGE SCRIPTS
 ======================================*/
 $('.fancybox-media').fancybox({
-openEffect: 'elastic',
-closeEffect: 'elastic',
-helpers: {
-title: {
-type: 'inside'
-}
-}
+	openEffect: 'elastic',
+	closeEffect: 'elastic',
+	helpers: {
+		title: {
+			type: 'inside'
+		}
+	}
 });
 
 
@@ -2381,30 +2371,29 @@ type: 'inside'
 FILTER FUNCTIONALITY SCRIPTS
 ======================================*/
 $(window).load(function () {
-var $container = $('#work-div');
-$container.isotope({
-filter: '*',
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-$('.caegories a').click(function () {
-$('.caegories .active').removeClass('active');
-$(this).addClass('active');
-var selector = $(this).attr('data-filter');
-$container.isotope({
-filter: selector,
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-return false;
-});
-
+	var $container = $('#work-div');
+	$container.isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 750,
+			easing: 'linear',
+			queue: false
+		}
+	});
+	$('.caegories a').click(function () {
+		$('.caegories .active').removeClass('active');
+		$(this).addClass('active');
+		var selector = $(this).attr('data-filter');
+		$container.isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false
+			}
+		});
+		return false;
+	});
 });
 
 
--- javascripts/custom-solid.js
+++ javascripts/custom-solid.js
@@ -21,21 +21,11 @@ SCROLLING SCRIPTS
 ======================================*/
 
 $('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
-var $anchor = $(this);
-$('html, body').stop().animate({
-scrollTop: $($anchor.attr('href')).offset().top
-}, 1200, 'easeInOutExpo');
-event.preventDefault();
-});
-
-
-/*====================================
-SLIDER SCRIPTS
-======================================*/
-
-
-$('#carousel-slider').carousel({
-interval: 2000 //TIME IN MILLI SECONDS
+	var $anchor = $(this);
+	$('html, body').stop().animate({
+		scrollTop: $($anchor.attr('href')).offset().top
+	}, 1200, 'easeInOutExpo');
+	event.preventDefault();
 });
 
 
@@ -43,13 +33,13 @@ interval: 2000 //TIME IN MILLI SECONDS
 POPUP IMAGE SCRIPTS
 ======================================*/
 $('.fancybox-media').fancybox({
-openEffect: 'elastic',
-closeEffect: 'elastic',
-helpers: {
-title: {
-type: 'inside'
-}
-}
+	openEffect: 'elastic',
+	closeEffect: 'elastic',
+	helpers: {
+		title: {
+			type: 'inside'
+		}
+	}
 });
 
 
@@ -57,30 +47,29 @@ type: 'inside'
 FILTER FUNCTIONALITY SCRIPTS
 ======================================*/
 $(window).load(function () {
-var $container = $('#work-div');
-$container.isotope({
-filter: '*',
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-$('.caegories a').click(function () {
-$('.caegories .active').removeClass('active');
-$(this).addClass('active');
-var selector = $(this).attr('data-filter');
-$container.isotope({
-filter: selector,
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-return false;
-});
-
+	var $container = $('#work-div');
+	$container.isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 750,
+			easing: 'linear',
+			queue: false
+		}
+	});
+	$('.caegories a').click(function () {
+		$('.caegories .active').removeClass('active');
+		$(this).addClass('active');
+		var selector = $(this).attr('data-filter');
+		$container.isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false
+			}
+		});
+		return false;
+	});
 });
 
 
